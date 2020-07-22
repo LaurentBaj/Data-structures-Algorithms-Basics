421bc00513ed21a7e5cf23254d2c57141a3c54af
LaurentBaj <laurent_baj@hotmail.com>
2020-07-22 22:36:42 +0200

    Finished Shell Sort
    Need to scan it once more tomorrow as to make sure I understood it properly

--- Section 3_Sort Algorithms/src/InsertionSort.java
+++ Section 3_Sort Algorithms/src/InsertionSort.java
@@ -15,10 +15,9 @@ public class InsertionSort {
             intArray[i] = newElement;
         }
 
+        // Output
         for (int i = 0; i < intArray.length; i++) {
             System.out.println(intArray[i]);
         }
-
-
     }
 }
